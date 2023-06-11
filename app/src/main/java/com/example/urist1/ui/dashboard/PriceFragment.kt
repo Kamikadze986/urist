398794fedbc28d189ce863cc2573b5cdfb8b2f88
Dmitriy-111 <d.pestrov@limehd.tv>
2023-06-11 18:52:37 +0300

    set price frragment

--- app/src/main/java/com/example/urist1/ui/dashboard/PriceFragment.kt
+++ app/src/main/java/com/example/urist1/ui/dashboard/PriceFragment.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.urist1.databinding.FragmentDashboardBinding
+import com.example.urist1.databinding.FragmentPriceBinding
 
-class DashboardFragment : Fragment() {
+class PriceFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentPriceBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +24,9 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentPriceBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
